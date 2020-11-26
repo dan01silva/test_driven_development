c6a3bf8a652fde69f2d899ced03f3a89309c5a82
Daniel Silva <daniel.silva1@allianz.com.br>
2020-11-26 00:50:51 -0200

    Filaziado Curso TDD

--- teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
+++ teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
@@ -15,6 +15,10 @@ public class Avaliador {
 	private List<Lance> maiores;
 
 	public void avalia(Leilao leilao) {
+		if(maiores.size() == 0) {
+			throw new RuntimeException("Não é possivel fazer um leilão sem lances");
+		}
+		
 		for(Lance lance : leilao.getLances()){
 			if(lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
 			if(lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
